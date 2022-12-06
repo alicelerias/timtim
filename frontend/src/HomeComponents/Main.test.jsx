3412d8b5aebd73fc8a4f53e2acbdead31f5b3376
Alice Lerias <leriasler@gmail.com>
2022-12-06 14:45:01 -0300

    fix test

--- frontend/src/HomeComponents/Main.test.jsx
+++ frontend/src/HomeComponents/Main.test.jsx
@@ -3,51 +3,41 @@ import React from 'react'
 import { render, fireEvent, screen } from '@testing-library/react'
 
 import Main from './Main'
+import TasksContext from '../context/tasks'
 
-// import MockedPanel from './Panel'
+const tasks = [
+  {
+    titulo: 'test',
+    id: 1,
+    epic: {
+      name: 'epic-1',
+      color: 'verde',
+    },
+    pontos: 10,
+    status: 'backlog',
+    prioridade: 'medium',
+  },
+
+  {
+    titulo: 'test',
+    id: 2,
+    epic: {
+      name: 'epic-1',
+      color: 'verde',
+    },
+    pontos: 10,
+    status: 'backlog',
+    prioridade: 'medium',
+  },
+]
 
 describe('tests for Main Component', () => {
-  jest.mock('./Panel', () => ({
-    Panel: () => {
-      const tasks = [
-        {
-          titulo: 'test',
-          id: 1,
-          epic: {
-            name: 'epic-1',
-            color: 'verde',
-          },
-          pontos: 10,
-          status: 'backlog',
-          prioridade: 'medium',
-        },
-
-        {
-          titulo: 'test',
-          id: 2,
-          epic: {
-            name: 'epic-1',
-            color: 'verde',
-          },
-          pontos: 10,
-          status: 'backlog',
-          prioridade: 'medium',
-        },
-      ]
-      return <mock-panel data-testid="panel" tasks={tasks} />
-    },
-  }))
-
-  it('test', async () => {
+  it('render Main component should work', async () => {
     const openModal = jest.fn()
-    const taskId = jest.fn()
-    const setTaskId = jest.fn()
-
-    const { getByTestId } = render(<Main openModal={openModal}></Main>)
-
-    const mainNode = screen.getByTestId('main-test-render')
-
-    fireEvent.click(mainNode)
-    expect(openModal).toHaveBeenCalledTimes(1)
+    render(
+      <TasksContext.Provider value={{ tasks }}>
+        <Main openModal={openModal}></Main>
+      </TasksContext.Provider>
+    )
   })
 })
